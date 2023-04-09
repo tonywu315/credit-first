0604b5db45e1205e3215bbd57596a54a34565f18
Tony Wu <tonywu315@gmail.com>
2023-04-09 04:32:57 -0400

    Recommendation

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -1,123 +1,123 @@
-import { useParams } from "react-router-dom"
-import { Buffer } from "buffer"
+import { useParams } from "react-router-dom";
+import { Buffer } from "buffer";
 
-import { Center, Image, HStack, VStack, Box, Container, Grid, Text } from "@chakra-ui/react";
+import {
+    Center,
+    Image,
+    HStack,
+    VStack,
+    Box,
+    Container,
+    Grid,
+    Text,
+} from "@chakra-ui/react";
 
-import cardsData from './../data/credit_cards.json';
+import cardsData from "./../data/credit_cards.json";
+import { ClassNames } from "@emotion/react";
+import classes from "./Recommend.module.css";
 
 export function Recommend() {
-  let params = useParams();
-  let userData = JSON.parse(Buffer.from(params.userData, "base64").toString()) // these are where the answers are
-  
-  userData.age = parseInt(userData.age)
-  userData.creditScore = parseInt(userData.creditScore)
-  userData.income = parseInt(userData.income)
-  userData.rent = parseInt(userData.rent)
-  userData.food = parseInt(userData.food)
-  userData.gas = parseInt(userData.gas)
-  userData.debt = parseInt(userData.debt)
-  userData.entertainment = parseInt(userData.entertainment)
+    let params = useParams();
+    let userData = JSON.parse(
+        Buffer.from(params.userData, "base64").toString()
+    ); // these are where the answers are
 
-  let availableNames = Object.keys(cardsData)
+    userData.age = parseInt(userData.age);
+    userData.creditScore = parseInt(userData.creditScore);
+    userData.income = parseInt(userData.income);
+    userData.rent = parseInt(userData.rent);
+    userData.food = parseInt(userData.food);
+    userData.gas = parseInt(userData.gas);
+    userData.debt = parseInt(userData.debt);
+    userData.entertainment = parseInt(userData.entertainment);
 
-  availableNames = availableNames.filter(name => {
-    let card = cardsData[name]
-    return ((card.student && userData.student) || !card.student)
-  })
+    let availableNames = Object.keys(cardsData);
 
+    availableNames = availableNames.filter((name) => {
+        let card = cardsData[name];
+        return (card.student && userData.student) || !card.student;
+    });
 
-  availableNames = availableNames.filter(name => {
-    let card = cardsData[name]
-    if (userData.creditScore == 0 || isNaN(userData.creditScore)) {
-      let predicted_score = 0.005524 * userData.income + 343.3
-      return (card.credit <= predicted_score)
-    }
-    return (card.credit <= userData.creditScore)
-  })
-
-
-  let maximizer = 0;
-
-
-  for (const name of availableNames) {
-    let card = cardsData[name]
-    let factor = 0
-    let total = 0
-    for (const source of card.cashback) {
-      let contribution = 0
-      for (const category of source.category) {
-        if (category != "all") {
-          contribution += (source.amount/100) * userData[category]
+    availableNames = availableNames.filter((name) => {
+        let card = cardsData[name];
+        if (userData.creditScore == 0 || isNaN(userData.creditScore)) {
+            let predicted_score = 0.005524 * userData.income + 343.3;
+            return card.credit <= predicted_score;
         }
-        else {
-          contribution += Math.max((source.amount/100)*userData["entertainment"], (source.amount/100)*userData["rent"], (source.amount/100)*userData["food"])
+        return card.credit <= userData.creditScore;
+    });
+
+    let maximizer = 0;
+
+    for (const name of availableNames) {
+        let card = cardsData[name];
+        let factor = 0;
+        let total = 0;
+        for (const source of card.cashback) {
+            let contribution = 0;
+            for (const category of source.category) {
+                if (category != "all") {
+                    contribution += (source.amount / 100) * userData[category];
+                } else {
+                    contribution += Math.max(
+                        (source.amount / 100) * userData["entertainment"],
+                        (source.amount / 100) * userData["rent"],
+                        (source.amount / 100) * userData["food"]
+                    );
+                }
+            }
+            contribution = Math.min(contribution, source.limit);
+            total += contribution;
         }
-      }
-      contribution = Math.min(contribution, source.limit)
-      total += contribution
+        factor = (userData.age - 18) / userData.debt + total - card.annual_fee;
+
+        maximizer = Math.max(factor, maximizer);
     }
-    factor = (userData.age-18)/userData.debt + total - card.annual_fee
 
+    // console.log(availableNames)
 
-    maximizer = Math.max(factor, maximizer)
-  }
+    let winner = null;
 
-  // console.log(availableNames)
-
-  let winner = null
-
-  for (const name of availableNames) {
-    let card = cardsData[name]
-    let factor = 0
-    let total = 0
-    for (const source of card.cashback) {
-      let contribution = 0
-      for (const category of source.category) {
-        if (category != "all") {
-          contribution += (source.amount/100) * userData[category]
+    for (const name of availableNames) {
+        let card = cardsData[name];
+        let factor = 0;
+        let total = 0;
+        for (const source of card.cashback) {
+            let contribution = 0;
+            for (const category of source.category) {
+                if (category != "all") {
+                    contribution += (source.amount / 100) * userData[category];
+                } else {
+                    contribution += Math.max(
+                        (source.amount / 100) * userData["entertainment"],
+                        (source.amount / 100) * userData["rent"],
+                        (source.amount / 100) * userData["food"]
+                    );
+                }
+            }
+            contribution = Math.min(contribution, source.limit);
+            total += contribution;
         }
-        else {
-          contribution += Math.max((source.amount/100)*userData["entertainment"], (source.amount/100)*userData["rent"], (source.amount/100)*userData["food"])
+        factor = (userData.age - 18) / userData.debt + total - card.annual_fee;
+        if (factor === maximizer) {
+            winner = card;
+            winner.name = name;
         }
-      }
-      contribution = Math.min(contribution, source.limit)
-      total += contribution
     }
-    factor = (userData.age-18)/userData.debt + total - card.annual_fee
-    if (factor === maximizer) {
-      winner = card
-      winner.name = name
-    }
-  }
 
-  console.log(winner.cashback.length)
+    console.log(winner.cashback.length);
 
-  return (
-    <div>
-      <Container h='100vh' maxWidth="container.lg">
-        <VStack spacing="10px">
-          <Box h='13vh' w='100vh' bg="tomato"></Box>
-          <Text align='left' fontSize='34px'>Our Best Choice</Text>
-          <Box h='45vh' w='100vh'>
-            <a href={winner.link} rel="noreferrer noopener" target="_blank">  
-            <VStack spacing='8px'>
-              <HStack maxWidth='50%' spacing='40px'>
-                <img src={winner.image}/>
-                <Text fontSize='28px' fontWeight='bold'>{winner.name}</Text>
-              </HStack>
-              
-              <div style={{display: 'flex', justifyContent: 'center'}}>
-                <div>
-                  
+    return (
+        <div className={classes.container}>
+            <p className={classes.title}>Our Recommendation {"temp"}</p>
+            <div className={classes.card}>
+                <div className={classes.image}>
+                    <img src="" alt="credit card" />
+                    <p>Discover</p>
                 </div>
-              </div>
-            </VStack>
-            </a>
-          </Box>
-          <Box h='30vh' w='100vh'></Box>
-          <Box h='5vh' w='100vh' bg="tomato"></Box>
-        </VStack>
-      </Container>
-    </div>
-  )
+                <div className={classes.info}></div>
+            </div>
+            <div className={classes.tips}></div>
+        </div>
+    );
 }
